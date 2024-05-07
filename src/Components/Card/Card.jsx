04d184ea90e65f861bfa91e5b33e407961214921
Santiago <penasantiago2030@gamilcom>
2024-05-07 17:43:05 -0500

    Structure Proyect

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div>
+        <h3>Cosa</h3>
+        <span>2.50</span>
+        <p>Libro</p>
+        <p>BSA DA</p>
+        <img></img>
+        <button>Añadir</button>
+    </div>
+  )
+}
